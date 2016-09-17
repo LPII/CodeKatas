899858d1db827a2080eb51c368d0950884b176cf
LPII <LancePorterGD@gmail.com>
2016-09-16 22:15:39 -0400

    Add console ver of plus pattern

---
+++ plusPattern.js
@@ -0,0 +1,50 @@
+/*
+
+Task: Raj once wanted to code a program to make a plus sign using the numbers but he didn't have any idea to do so. Using 'n' as a parameter in the function pattern complete the code and solve the pattern so that Raj fells happy.
+
+
+console output version
+
+*/
+
+function pattern(n) {
+  var limit = n;
+  var verLim = (n * 2) - 1;
+  var whSp = " ";
+  var repWhSp;
+  var seqArr = [];
+  var theSeq;
+
+  function retSeq(int) {
+    for (var j = 1; j <= verLim; j++) {
+      if (j <= limit) {
+        seqArr.push(j);
+      } else {
+        g = ((j - n) - n) * -1;
+        seqArr.push(g);
+      }
+    }
+  } //end retSeq
+  
+function repWp(li){
+var val = li;
+var ar = Array(val).join(" ");
+return ar;
+}//end repWp
+
+  retSeq(limit);
+  theSeq = seqArr.join("");
+  repWhSp = repWp(limit);
+
+  for (var i = 1; i <= verLim; i++) {
+    if (i == limit) {
+      console.log(theSeq);
+    } else if (i <= limit) {
+      console.log(repWhSp + i)
+    } else {
+      console.log(repWhSp + (((i - n) - n) * -1));
+    }
+  } //end for 
+}
+
+pattern(3);
